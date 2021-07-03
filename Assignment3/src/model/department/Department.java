84a37be4f66bfc4137cd6478aa6be592228a4094
DikshaGodse <Hp@DESKTOP-0AHR00P>
2021-07-03 12:35:31 +0530

    Assignment 3 : University model

--- Assignment3/src/model/department/Department.java
+++ Assignment3/src/model/department/Department.java
@@ -3,16 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.university;
+package model.department;
 
 import java.util.List;
-import model.department.Department;
+import model.course.Course;
 
 /**
  *
  * @author Hp
  */
-public class University {
-    private String universityName;
-    private List<Department> departmentList;
+public class Department {
+    private String departmentName;
+    private List<Course> courseList;
 }
